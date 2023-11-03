638929b6ac6f67bd6f70f02b0d7fcd45cee48f3e
kazaz03 <148658131+kazaz03@users.noreply.github.com>
2023-11-03 21:33:24 +0100

    Update FiksniBroj.java

--- lv3-z1/src/ba/unsa/etf/rpr/FiksniBroj.java
+++ lv3-z1/src/ba/unsa/etf/rpr/FiksniBroj.java
@@ -1,13 +1,4 @@
 package ba.unsa.etf.rpr;
-/*
-● FiksniBroj sadrži konstruktor FiksniBroj(Grad grad, String broj). Parametar
-broj je dio telefonskog broja bez pozivnog broja npr. "123-456", a grad je
-promjenljiva pobrojanog tipa koji označava pozivni broj koji treba koristiti npr.:
-enum Grad { SARAJEVO, TUZLA, ZENICA… } Spisak pozivnih brojeva se
-može naći ovdje. Umjesto imena kantona koristite ime glavnog grada kantona
-(kao u primjeru iznad), a umjesto Brčko distrikta stavite BRCKO. Metoda ispisi()
-treba vratiti broj oblika "033/123-456".
- */
 
 public class FiksniBroj extends TelefonskiBroj{
     private
